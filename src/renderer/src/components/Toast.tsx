d86792b8d9db02551e83c3835d0d4f08ef1006cb
grokzero0 <grok1618@gmail.com>
2026-01-07 22:22:40 -0500

    add useeffect for toast

--- src/renderer/src/components/Toast.tsx
+++ src/renderer/src/components/Toast.tsx
@@ -1,20 +1,23 @@
 import { X } from 'lucide-react'
-import { JSX } from 'react'
+import { JSX, useEffect } from 'react'
 import { toast } from 'sonner'
 import { Toaster } from './ui/sonner'
 
 function Toast(): JSX.Element {
-  window.electronAPI.toastMessage((message, description) => {
-    toast(message, {
-      action: {
-        label: <X />,
-        onClick: () => {
-          return
-        }
-      },
-      description: description
+  useEffect(() => {
+    window.electronAPI.toastMessage((message, description) => {
+      toast(message, {
+        action: {
+          label: <X />,
+          onClick: () => {
+            return
+          }
+        },
+        description: description
+      })
     })
-  })
+    return () => window.electronAPI.clearAllListeners('toast-message')
+  }, [])
   return <Toaster />
 }
 
