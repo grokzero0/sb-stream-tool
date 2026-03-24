9dcb44dd7e86b6dfa3cab14d1fb16340f1f14e9c
grokzero0 <grok1618@gmail.com>
2026-03-24 18:59:15 -0400

    add icons

--- packages/main/src/modules/WindowManager.ts
+++ packages/main/src/modules/WindowManager.ts
@@ -1,24 +1,31 @@
-import type {AppModule} from '../AppModule.js';
-import {ModuleContext} from '../ModuleContext.js';
-import {BrowserWindow} from 'electron';
-import type {AppInitConfig} from '../AppInitConfig.js';
+import type { AppModule } from "../AppModule.js";
+import { ModuleContext } from "../ModuleContext.js";
+import { BrowserWindow } from "electron";
+import type { AppInitConfig } from "../AppInitConfig.js";
+import { join } from "path";
 
 class WindowManager implements AppModule {
-  readonly #preload: {path: string};
-  readonly #renderer: {path: string} | URL;
+  readonly #preload: { path: string };
+  readonly #renderer: { path: string } | URL;
   readonly #openDevTools;
 
-  constructor({initConfig, openDevTools = false}: {initConfig: AppInitConfig, openDevTools?: boolean}) {
+  constructor({
+    initConfig,
+    openDevTools = false,
+  }: {
+    initConfig: AppInitConfig;
+    openDevTools?: boolean;
+  }) {
     this.#preload = initConfig.preload;
     this.#renderer = initConfig.renderer;
     this.#openDevTools = openDevTools;
   }
 
-  async enable({app}: ModuleContext): Promise<void> {
+  async enable({ app }: ModuleContext): Promise<void> {
     await app.whenReady();
     await this.restoreOrCreateWindow(true);
-    app.on('second-instance', () => this.restoreOrCreateWindow(true));
-    app.on('activate', () => this.restoreOrCreateWindow(true));
+    app.on("second-instance", () => this.restoreOrCreateWindow(true));
+    app.on("activate", () => this.restoreOrCreateWindow(true));
   }
 
   async createWindow(): Promise<BrowserWindow> {
@@ -31,6 +38,7 @@ class WindowManager implements AppModule {
         webviewTag: false, // The webview tag is not recommended. Consider alternatives like an iframe or Electron's BrowserView. @see https://www.electronjs.org/docs/latest/api/webview-tag#warning
         preload: this.#preload.path,
       },
+      icon: join(import.meta.dirname, "..", "src", "assets", "icon.ico"),
     });
 
     if (this.#renderer instanceof URL) {
@@ -43,7 +51,7 @@ class WindowManager implements AppModule {
   }
 
   async restoreOrCreateWindow(show = false) {
-    let window = BrowserWindow.getAllWindows().find(w => !w.isDestroyed());
+    let window = BrowserWindow.getAllWindows().find((w) => !w.isDestroyed());
 
     if (window === undefined) {
       window = await this.createWindow();
@@ -67,9 +75,10 @@ class WindowManager implements AppModule {
 
     return window;
   }
-
 }
 
-export function createWindowManagerModule(...args: ConstructorParameters<typeof WindowManager>) {
+export function createWindowManagerModule(
+  ...args: ConstructorParameters<typeof WindowManager>
+) {
   return new WindowManager(...args);
 }
