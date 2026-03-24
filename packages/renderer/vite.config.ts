993ad4fa78c0b8534d0c4519567df71db2122b45
grokzero0 <grok1618@gmail.com>
2026-03-24 18:41:42 -0400

    add shadcn

--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
+  resolve: {
+    alias: {
+      "@renderer": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
