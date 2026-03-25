f76dc7ffe5ccdfcedb38c1b9d92b1624c7a82bef
grokzero0 <grok1618@gmail.com>
2026-03-25 10:25:42 -0400

    add apollo query files

---
+++ packages/renderer/src/utils/queries.ts
@@ -0,0 +1,172 @@
+import { gql } from "@apollo/client";
+export const GET_SET_INFO = gql`
+  query SetEntrants($setId: ID!) {
+    set(id: $setId) {
+      id
+      fullRoundText
+      event {
+        name
+        tournament {
+          name
+        }
+        videogame {
+          id
+          displayName
+        }
+      }
+      slots {
+        id
+        entrant {
+          id
+          name
+          participants {
+            id
+            prefix
+            gamerTag
+            user {
+              genderPronoun
+              authorizations(types: TWITTER) {
+                externalUsername
+              }
+            }
+            connectedAccounts
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_ENTRANTS_INFO = gql`
+  query EntrantsByVideogameInTournament(
+    $tourneySlug: String
+    $videogameId: [ID]
+  ) {
+    tournament(slug: $tourneySlug) {
+      id
+      name
+      events(filter: { videogameId: $videogameId }) {
+        id
+        name
+        entrants(query: { perPage: 70, page: 1 }) {
+          nodes {
+            id
+            participants {
+              id
+              prefix
+              gamerTag
+              user {
+                genderPronoun
+                authorizations(types: TWITTER) {
+                  externalUsername
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_LIVE_SETS = gql`
+  query LiveEventSets($eventSlug: String!, $page: Int!, $perPage: Int!) {
+    event(slug: $eventSlug) {
+      id
+      name
+      tournament {
+        name
+      }
+      sets(
+        page: $page
+        perPage: $perPage
+        sortType: STANDARD
+        filters: { state: [1], hideEmpty: true }
+      ) {
+        pageInfo {
+          total
+          totalPages
+        }
+        nodes {
+          # id
+          fullRoundText
+          state
+          stream {
+            streamSource
+            streamName
+          }
+          slots {
+            id
+            entrant {
+              id
+              name
+              participants {
+                id
+                prefix
+                gamerTag
+                user {
+                  genderPronoun
+                  authorizations(types: TWITTER) {
+                    externalUsername
+                  }
+                }
+                connectedAccounts
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_SETS_IN_EVENT = gql`
+  query EventSets($eventSlug: String!, $page: Int!, $perPage: Int!) {
+    event(slug: $eventSlug) {
+      id
+      name
+      tournament {
+        name
+      }
+      sets(
+        page: $page
+        perPage: $perPage
+        sortType: STANDARD
+        filters: { hideEmpty: true }
+      ) {
+        pageInfo {
+          total
+          totalPages
+        }
+        nodes {
+          # id
+          fullRoundText
+          state
+          stream {
+            streamSource
+            streamName
+          }
+          slots {
+            id
+            entrant {
+              id
+              name
+              participants {
+                id
+                prefix
+                gamerTag
+                user {
+                  genderPronoun
+                  authorizations(types: TWITTER) {
+                    externalUsername
+                  }
+                }
+                connectedAccounts
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
