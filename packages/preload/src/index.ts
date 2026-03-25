5f3e79de6a2f3a05acff00893bca2ade2eed5c4d
grokzero0 <grok1618@gmail.com>
2026-03-25 10:50:39 -0400

    add preload

--- packages/preload/src/index.ts
+++ packages/preload/src/index.ts
@@ -1,9 +1,55 @@
-import {sha256sum} from './nodeCrypto.js';
-import {versions} from './versions.js';
-import {ipcRenderer} from 'electron';
+// import { sha256sum } from "./nodeCrypto.js";
+// import { versions } from "./versions.js";
+import { Tournament } from "@app/common/src/types.js";
+import { ipcRenderer } from "electron";
 
-function send(channel: string, message: string) {
-  return ipcRenderer.invoke(channel, message);
+function send(channel: string, ...args: any[]) {
+  return ipcRenderer.invoke(channel, ...args);
 }
 
-export {sha256sum, versions, send};
+function navigation(callback: (location: string) => void) {
+  ipcRenderer.on("navigation", (_event, location) => callback(location));
+}
+
+function toastMessage(
+  callback: (message?: string, description?: string) => void,
+) {
+  ipcRenderer.on("toast-message", (_event, message, description) =>
+    callback(message, description),
+  );
+}
+
+function updateOverlay(data: Tournament) {
+  return ipcRenderer.invoke("overlay/update", data);
+}
+
+function clearAllListeners(channel: string) {
+  ipcRenderer.removeAllListeners(channel);
+}
+
+// function onNewSlippiGameData(callback: (data: SlippiGameData) => void) {
+//   ipcRenderer.on("slippi:new-game-start-data", (_event, data) =>
+//     callback(data),
+//   );
+// }
+
+// function onNewSlippiGameEndData(callback: (winner: number) => void) {
+//   ipcRenderer.on("slippi:new-game-end-data", (_event, winner) =>
+//     callback(winner),
+//   );
+// }
+
+// function autoStopSlippiRelay() {
+//   return ipcRenderer.invoke("slippi:autoStopReadingFolder");
+// }
+
+export {
+  send,
+  updateOverlay,
+  navigation,
+  toastMessage,
+  // onNewSlippiGameData,
+  // onNewSlippiGameEndData,
+  // autoStopSlippiRelay,
+  clearAllListeners,
+};
