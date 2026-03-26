cd61665d54233e0ce52d90b26a3b0d012816a746
grokzero0 <grok1618@gmail.com>
2026-03-26 16:16:40 -0400

    add alert reminder

--- packages/renderer/src/components/Match.tsx
+++ packages/renderer/src/components/Match.tsx
@@ -1,23 +1,38 @@
+import { useSettingsStore } from "@renderer/zustand/store";
 import Commentators from "./Commentators";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
+import { Alert, AlertTitle } from "./ui/alert";
+import { AlertCircleIcon } from "lucide-react";
 // import SetQuery from "./SetQuery";
 
 function Match() {
+  const apiKey = useSettingsStore((state) => state.apiKey);
   return (
-    <form>
-      <Tabs defaultValue="players">
-        <TabsList className="w-full">
-          <TabsTrigger value="players">Players</TabsTrigger>
-          <TabsTrigger value="commentators">Commentators</TabsTrigger>
-        </TabsList>
-        <TabsContent value="players">
-          <>player</>
-        </TabsContent>
-        <TabsContent value="commentators">
-          <Commentators />
-        </TabsContent>
-      </Tabs>
-    </form>
+    <div className="flex flex-col gap-2">
+      {apiKey === "" && (
+        <Alert>
+          <AlertCircleIcon />
+          <AlertTitle>
+            You must have a start.gg api key in order to use the automated set
+            fetching tools (go to settings to set it!)
+          </AlertTitle>
+        </Alert>
+      )}
+      <form>
+        <Tabs defaultValue="players">
+          <TabsList className="w-full">
+            <TabsTrigger value="players">Players</TabsTrigger>
+            <TabsTrigger value="commentators">Commentators</TabsTrigger>
+          </TabsList>
+          <TabsContent value="players">
+            <>player</>
+          </TabsContent>
+          <TabsContent value="commentators">
+            <Commentators />
+          </TabsContent>
+        </Tabs>
+      </form>
+    </div>
   );
 }
 
