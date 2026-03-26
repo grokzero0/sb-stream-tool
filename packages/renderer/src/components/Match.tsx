fa810b994c4122a1b0aea2740194af9eb244e63f
grokzero0 <grok1618@gmail.com>
2026-03-26 16:13:52 -0400

    add tabs

--- packages/renderer/src/components/Match.tsx
+++ packages/renderer/src/components/Match.tsx
@@ -1,8 +1,24 @@
 import Commentators from "./Commentators";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
 // import SetQuery from "./SetQuery";
 
 function Match() {
-  return <Commentators />;
+  return (
+    <form>
+      <Tabs defaultValue="players">
+        <TabsList className="w-full">
+          <TabsTrigger value="players">Players</TabsTrigger>
+          <TabsTrigger value="commentators">Commentators</TabsTrigger>
+        </TabsList>
+        <TabsContent value="players">
+          <>player</>
+        </TabsContent>
+        <TabsContent value="commentators">
+          <Commentators />
+        </TabsContent>
+      </Tabs>
+    </form>
+  );
 }
 
 export default Match;
