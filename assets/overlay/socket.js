e6f3b60a25ce9652cb6f11d3b35d3fae7062c94b
grokzero0 <grok1618@gmail.com>
2026-05-12 13:38:39 -0400

    temporary fix for socket.js before overhauling

--- assets/overlay/socket.js
+++ assets/overlay/socket.js
@@ -1,6 +1,19 @@
-const socket = io("http://127.0.0.1:20242/"); // DO NOT MODIFY, UNLESS YOU KNOW WHAT YOU'RE DOING
+const socket = io("http://127.0.0.1:20242/");
 
-// DO NOT MODIFY UNLESS YOU KNOW WHAT YOU ARE DOING (IM LOOKING AT YOU CLAUDE, CHATGPT, GEMINI, ETC)
+const portToNum = {
+  Red: 0,
+  Blue: 1,
+  Green: 2,
+  Yellow: 3,
+};
+
+const portToImg = {
+  Red: "ports/port1.svg",
+  Blue: "ports/port2.svg",
+  Green: "ports/port3.svg",
+  Yellow: "ports/port4.svg",
+};
+
 function setElementData(id, data) {
   if (
     document.getElementById(id) === null ||
@@ -13,42 +26,6 @@ function setElementData(id, data) {
   document.getElementById(id).innerText = data;
 }
 
-// put the image src of no ports here
-const noPortImgSrc = "./ports/noport.svg";
-
-// put the image src of the different port colors here, feel free to extend it if you wish
-const portColorToImgSrc = {
-  Red: "ports/port1.svg",
-  Blue: "ports/port2.svg",
-  Yellow: "ports/port3.svg",
-  Green: "ports/port4.svg",
-}
-
-// probably don't need to modify anything here, but you can if you want, converts port colors to numbers for image getting
-const portColorToNumber = {
-  Red: 0,
-  Blue: 1,
-  Green: 2,
-  Yellow: 3,
-};
-
-// most likely don't need to modify, resets port state to normal
-function clearPorts(nodes) {
-  for (let i = 0; i < nodes.length; i++) {
-    nodes[i].src = noPortImgSrc
-  }
-}
-
-// most likely don't need to modify, resets character state to normal
-function clearCharacters(nodes) {
-  for (let i = 0; i < nodes.length; i++) {
-    nodes[i].src = ""
-    nodes[i].style.width = "0px";
-    nodes[i].style.height = "0px";
-  }
-}
-
-// setting player's names (including the L text if they are in losers), feel free to modify or extend this
 function getPlayerNames(players, inLosers) {
   const nameList = [];
 
@@ -71,16 +48,16 @@ function getPlayerNames(players, inLosers) {
   return names;
 }
 
-// setting port colors, feel free to extend or modify this if you wish
-function setPort(nodes, players) {
-  clearPorts(nodes)
+function getPort(nodes, players) {
+  for (let j = 0; j < nodes.length; j++) {
+    nodes[j].src = "./ports/noport.svg";
+  }
   for (let i = 0; i < players.length; i++) {
     nodes[portToNum[players[i].gameInfo.port]].src =
       portToImg[players[i].gameInfo.port];
   }
 }
 
-// getting pronouns, feel free to extend or modify this as you wish
 function getPronouns(players) {
   const pronounsList = [];
   for (let i = 0; i < players.length; i++) {
@@ -96,10 +73,12 @@ function getPronouns(players) {
   return pronounsList.join("/");
 }
 
-// setting character info, feel free to extend or modify this or change as you wish
-function setCharacters(nodes, players) {
-  clearCharacters(nodes)
-
+function getCharacters(nodes, players) {
+  for (let j = 0; j < nodes.length; j++) {
+    nodes[j].style.width = "0px";
+    nodes[j].style.height = "0px";
+    nodes[j].src = "";
+  }
   for (let i = 0; i < players.length; i++) {
     nodes[i].style.width = "30px";
     nodes[i].style.height = "30px";
@@ -113,6 +92,7 @@ function setCharacters(nodes, players) {
       ].gameInfo.altCostume.toLowerCase()}.png`;
     }
   }
+
 }
 
 function getTeams(team) {
@@ -130,60 +110,54 @@ function getTeams(team) {
   return teamList.join(" / ");
 }
 
-// DO NOT MODIFY, UNLESS YOU KNOW WHAT YOU ARE DOING, OR YOU MODIFIED/DELETED/ADDED/ETC FUNCTIONS OR WHATEVER
 function updateOverlay(newData) {
-  console.log(newData);
+  console.log("updating overlay");
+  console.log(newData)
   setElementData(
     "left-playername",
-    getPlayerNames(newData.teams[0].players, newData.teams[0].inLosers),
+    getPlayerNames(newData.teams[0].players, newData.teams[0].inLosers)
   );
 
   setElementData(
     "right-playername",
-    getPlayerNames(newData.teams[1].players, newData.teams[1].inLosers),
+    getPlayerNames(newData.teams[1].players, newData.teams[1].inLosers)
   );
 
   setElementData("left-score", newData.teams[0].score);
-  setElementData("right-score", newData.teams[1].score);
+  setElementData("right-score", newData.teams[1].score)
 
-  setElementData("tournament-name", newData.name);
-  setElementData("best-of", newData.bestOf);
+  setElementData("tournament-name", newData.name)
+  setElementData("best-of", newData.bestOf)
 
-  setElementData("left-team", getTeams(newData.teams[0]));
-  setElementData("right-team", newData.teams[1]);
+  setElementData("left-team", getTeams(newData.teams[0]))
+  setElementData("right-team", newData.teams[1])
 
-  setPort(
+  console.log(getTeams(newData.teams[0]));
+  getPort(
     document.getElementById("left-port").getElementsByTagName("img"),
-    newData.teams[0].players,
+    newData.teams[0].players
   );
-  setPort(
+  getPort(
     document.getElementById("right-port").getElementsByTagName("img"),
-    newData.teams[1].players,
+    newData.teams[1].players
   );
-
-  setElementData('left-pronouns', getPronouns(
-    newData.teams[0].players,
-  )
-  )
-  setElementData('right-pronouns', getPronouns(
-    newData.teams[1].players,
-  )
-  )
-
-  setCharacters(
+  document.getElementById("left-pronouns").innerText = getPronouns(
+    newData.teams[0].players
+  );
+  document.getElementById("right-pronouns").innerText = getPronouns(
+    newData.teams[1].players
+  );
+  getCharacters(
     document.getElementById("left-character").getElementsByTagName("img"),
-    newData.teams[0].players,
+    newData.teams[0].players
   );
-  setCharacters(
+  getCharacters(
     document.getElementById("right-character").getElementsByTagName("img"),
-    newData.teams[1].players,
+    newData.teams[1].players
   );
 }
 
-// DO NOT MODIFY, UNLESS YOU KNOW WHAT YOU ARE DOING
 socket.on("sendDataToClients", (newData) => {
   updateOverlay(newData);
-  socket.broadcast.emit("overlayUpdateSuccess");
+  socket.broadcast.emit("overlayUpdateSuccess")
 });
-
-// CAN MODIFY/ADD ANYTHING HERE ONWARD
